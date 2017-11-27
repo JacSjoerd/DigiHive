4a90549726d2f5bbe21feafd89e40d761d03b828
JacSjoerd <sjoerd@sjoerd.com>
2017-11-27 22:10:12 +0100

    Add Voltage Reading
    Feature to read the voltage on the ESP8266.

--- mqtt.cpp
+++ mqtt.cpp
@@ -39,68 +39,7 @@ void MQTT::initialize()
   }
 }
 
-void MQTT::initialize(const char* wifiSSID, const char* wifiPassword)
-{
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  // WiFiManager wifiManager;
-  // wifiManager.resetSettings();
-  // wifiManager.autoConnect("DigiHive");
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(wifiSSID);
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(wifiSSID, wifiPassword);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-     switch (WiFi.status()) {
-      case WL_NO_SHIELD:
-        Serial.println("No ESP8266 shield available.");
-        break;
-      case WL_IDLE_STATUS:
-        Serial.println("Nothing to do, waiting.");
-        break;
-      case WL_NO_SSID_AVAIL:
-        Serial.println("Can't find network (No SSID)");
-        break;
-      case WL_SCAN_COMPLETED:
-        Serial.println("Scan completed");
-        break;
-      case WL_CONNECTED:
-        Serial.println("Connected.");
-        break;
-      case WL_CONNECT_FAILED:
-        Serial.println("Failed to connect.");
-        break;
-      case WL_CONNECTION_LOST:
-        Serial.println("Connection lost.");
-        break;
-      case WL_DISCONNECTED:
-        Serial.println("Disconnected");
-        break;
-     }
-  }
-  Serial.println();
 
-  Serial.println("Connected to Wifi...");
-
-  setServer(mqttServer, mqttPort);
-
-  char myChipId[10] = "";
-  sprintf(myChipId, "%06X", ESP.getChipId());
-  sprintf(arduinoClientName, "DigiHive_%s", myChipId);
-
-  // Put the ESP ID into the topics
-  for (int i = 0; i < NUM_TOPICS; i++) {
-    for (int j = 0; j < 6; j++) {
-      topicList[i][j] = myChipId[j];
-    }
-  }
-}
- 
 int MQTT::reconnect()
 {
   // Loop until we're reconnected
